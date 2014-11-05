178c2baf53ee3ac944cc620eb04a2c7be7d94e17
Joselyn McDonald <mcdoj494@newschool.edu>
2014-11-05 15:38:41 -0600

    first commit

---
+++ Documents/openFrameworks/apps/myApps/mesh_with_geodes/src/of_tri.cpp
@@ -0,0 +1,9 @@
+//
+//  of_tri.cpp
+//  mesh_with_geodes
+//
+//  Created by Joselyn McDonald  on 11/5/14.
+//
+//
+
+#include "of_tri.h"
---
+++ Documents/openFrameworks/apps/myApps/mesh_with_geodes/src/of_tri.h
@@ -0,0 +1,14 @@
+//
+//  of_tri.h
+//  mesh_with_geodes
+//
+//  Created by Joselyn McDonald  on 11/5/14.
+//
+//
+
+#ifndef __mesh_with_geodes__of_tri__
+#define __mesh_with_geodes__of_tri__
+
+#include <iostream>
+
+#endif /* defined(__mesh_with_geodes__of_tri__) */
